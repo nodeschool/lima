503b5dd178d1621679ec77191f0dba7844fb551f
Lupo Montero <lupomontero@gmail.com>
2018-10-22 23:32:25 -0500

    Updates

---
+++ precache-manifest.378e0d227ce275411425c0af2e235e63.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5629c8656b2d4613b8f28dcb675212a8",
+    "url": "/lima/static/media/logo.5629c865.svg"
+  },
+  {
+    "revision": "07b9d5ad7d97dd694a12ef2e105ba268",
+    "url": "/lima/static/media/limajs-grupo-sept-2018.07b9d5ad.jpg"
+  },
+  {
+    "revision": "d9661ef4390dbbaeaca1645825a0826a",
+    "url": "/lima/static/media/bg.d9661ef4.png"
+  },
+  {
+    "revision": "103d6c6304a29c46067e",
+    "url": "/lima/static/js/runtime~main.103d6c63.js"
+  },
+  {
+    "revision": "a578f54fcad1e96c7beb",
+    "url": "/lima/static/js/main.a578f54f.chunk.js"
+  },
+  {
+    "revision": "ca250b1f15da07a3be0f",
+    "url": "/lima/static/js/1.ca250b1f.chunk.js"
+  },
+  {
+    "revision": "a578f54fcad1e96c7beb",
+    "url": "/lima/static/css/main.56e429c7.chunk.css"
+  },
+  {
+    "revision": "1bf3b5134dd1e76c9fba4d43df5fdc7c",
+    "url": "/lima/index.html"
+  }
+];
