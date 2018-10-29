c653ec9b0af78fbd17c62831abe300b054229d1f
Lupo Montero <lupomontero@gmail.com>
2018-10-29 10:56:51 -0500

    chore(components/Events): limpieza...

--- src/components/__tests__/Events.spec.js
+++ src/components/__tests__/Events.spec.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { render, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import Events from '../Events';
 
 
 it('shallow renders without crashing', () => {
   const spy = jest.spyOn(console, 'error').mockImplementation(() => null);
   const wrapper = mount(<Events />);
-  // console.log(wrapper);
   wrapper.unmount();
   spy.mockRestore();
 });
