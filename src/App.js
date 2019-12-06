b28da0f81b479d695c4a05e8dd3eee6b4aaf84dd
Lupo Montero <lupomontero@gmail.com>
2019-12-06 12:57:24 -0500

    Fixes bug introduced by router assuming page was hosted on domain's root dir

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Footer from './components/Footer';
 const App = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path='/'>
+      <Route path='/'>
         <Header />
         <About />
         <Events />
